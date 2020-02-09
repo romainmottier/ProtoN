3cc3c1570f0779ebf8f2590b7711a45f1a2f677b
Omar Durán <omaryesiduran@gmail.com>
2020-02-09 13:26:24 +0100

    Computing energy for Newmark scheme.

--- apps/fitted_acoustic_waves/fitted_acoustic_waves.cpp
+++ apps/fitted_acoustic_waves/fitted_acoustic_waves.cpp
@@ -50,9 +50,12 @@ using namespace Eigen;
 ////////////////////////// @omar::Begining:: simple fitted implementation /////////////////////////////////////////////
 
 #define fancy_stabilization_Q
+#define compute_energy_Q
 #define spatial_errors_Q
 //#define quadratic_time_solution_Q
 //#define quadratic_space_solution_Q
+
+double ComputeEnergy(poly_mesh<double> & msh, hho_degree_info & hho_di, assembler<poly_mesh<double>> & assembler, Matrix<double, Dynamic, 1> & p, Matrix<double, Dynamic, 1> & v);
 
 int main(int argc, char **argv)
 {
@@ -192,6 +195,15 @@ int main(int argc, char **argv)
     p_dof_n = analysis.solve(mass_assembler_p.RHS); // Initial scalar
     v_dof_n = analysis.solve(mass_assembler_v.RHS); // Initial velocity
     a_dof_n = analysis.solve(mass_assembler_a.RHS); // Initial acceleration
+    
+#ifdef compute_energy_Q
+        Matrix<RealType, Dynamic, 2> energy_h_values(nt+1,2);
+        energy_h_values(0,0) = 0.0;
+        energy_h_values(0,1) = 1.0;
+        RealType energy_h0 = ComputeEnergy(msh, hho_di, mass_assembler_a, p_dof_n, v_dof_n);
+        std::cout << bold << cyan << "Initial energy computed: " << tc << " seconds" << reset << std::endl;
+#endif
+    
     tc.toc();
     
     { // rendering projected acceleration
@@ -250,7 +262,6 @@ int main(int argc, char **argv)
         
         RealType beta = 0.25;
         RealType gamma = 0.5;
-        
         for(size_t it = 1; it <= nt; it++){
                 
             std::cout << bold << yellow << "Time step number : " << it << " being executed." << reset << std::endl;
@@ -437,6 +448,16 @@ int main(int argc, char **argv)
             }
 #endif
             
+#ifdef compute_energy_Q
+            {
+                tc.tic();
+                RealType energy_h = ComputeEnergy(msh, hho_di, assembler, p_dof_n, v_dof_n);
+                tc.toc();
+                energy_h_values(it,0) = t;
+                energy_h_values(it,1) = energy_h/energy_h0;
+                std::cout << bold << cyan << "Energy computed: " << tc << " seconds" << reset << std::endl;
+            }
+#endif
             
             // Rendering silo file
             auto num_cells = msh.cells.size();
@@ -468,10 +489,71 @@ int main(int argc, char **argv)
                     silo.add_variable("mesh", "uh", approx_u.data(), approx_u.size(), zonal_variable_t);
             silo.close();
         }
+        
+        #ifdef compute_energy_Q
+            std::cout << bold << cyan << "Reporting energy values : " << energy_h_values << reset << std::endl;
+            std::cout << bold << cyan << "Reporting initial energy value : " << energy_h0 << reset << std::endl;
+        #endif
+        
     }
-
+    
     return 0;
 }
+
+double ComputeEnergy(poly_mesh<double> & msh, hho_degree_info & hho_di, assembler<poly_mesh<double>> & assembler, Matrix<double, Dynamic, 1> & p_dof_n, Matrix<double, Dynamic, 1> & v_dof_n){
+    using RealType = double;
+    RealType energy_h = 0.0;
+    size_t cell_i = 0;
+    
+    double t = 0.0
+    ;    auto exact_sol_fun = [&t](const typename poly_mesh<double>::point_type& pt) -> double {
+
+#ifdef quadratic_time_solution_Q
+                return t * t * std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+#else
+#ifdef quadratic_space_solution_Q
+                return std::cos(std::sqrt(2.0)*M_PI*t) * (1.0-pt.x())*pt.x() * (1.0-pt.y())*pt.y();
+#else
+                return (1.0/(std::sqrt(2.0)*M_PI))*std::sin(std::sqrt(2.0)*M_PI*t) * std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+#endif
+#endif
+    };
+    
+    for (auto &cell : msh.cells) {
+        
+            cell_basis<poly_mesh<RealType>, RealType> cell_basis(msh, cell, hho_di.cell_degree());
+            auto cell_dof = cell_basis.size();
+
+            Matrix<RealType, Dynamic, Dynamic> mass = make_cell_mass_matrix(msh, cell, hho_di);
+
+            Matrix<RealType, Dynamic, Dynamic> cell_mass = mass.block(0, 0, cell_dof, cell_dof);
+            Matrix<RealType, Dynamic, 1> cell_alpha_dof_n_v = v_dof_n.block(cell_i*cell_dof, 0, cell_dof, 1);
+            
+            Matrix<RealType, Dynamic, 1> cell_mass_tested = cell_mass * cell_alpha_dof_n_v;
+            Matrix<RealType, 1, 1> term_1 = cell_alpha_dof_n_v.transpose() * cell_mass_tested;
+            energy_h += term_1(0,0);
+            
+            auto reconstruction_operator = make_hho_laplacian(msh, cell, hho_di);
+#ifdef fancy_stabilization_Q
+            auto stabilization_operator = make_hho_fancy_stabilization(msh, cell, reconstruction_operator.first, hho_di);
+#else
+            auto stabilization_operator = make_hho_naive_stabilization(msh, cell, hho_di);
+#endif
+            Matrix<RealType, Dynamic, Dynamic> laplacian_loc = reconstruction_operator.second + stabilization_operator;
+            Matrix<RealType, Dynamic, 1> cell_p_dofs = assembler.take_local_data(msh, cell, p_dof_n, exact_sol_fun);
+            Matrix<RealType, Dynamic, 1> cell_stiff_tested = laplacian_loc * cell_p_dofs;
+            Matrix<RealType, 1, 1> term_2 = cell_p_dofs.transpose() * cell_stiff_tested;
+            energy_h += term_2(0,0);
+        cell_i++;
+    }
+
+    std::cout << green << "Energy_h = " << std::endl << energy_h << std::endl;
+    std::cout << std::endl;
+    
+    return energy_h;
+}
+
+
 
 
 
