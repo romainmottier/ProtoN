f959225bd102977f4666af88755dadcd909bdaac
Romain Mottier <romain.mottier@outlook.com>
2025-03-01 15:57:04 +0100

    Update Elliptic_Interface_Agglo.cpp

--- apps/Unfitted_HHO/Elliptic_Problem/Agglo/Elliptic_Interface_Agglo.cpp
+++ apps/Unfitted_HHO/Elliptic_Problem/Agglo/Elliptic_Interface_Agglo.cpp
@@ -134,8 +134,8 @@ void CutHHOSecondOrderConvTest (int argc, char **argv) {
     RealType radius = 1.0/3.0;  
     // auto level_set_function = line_level_set<RealType>(line_y);
     // auto level_set_function = square_level_set<RealType>(0.77, 0.23, 0.23, 0.77);
-    auto level_set_function = circle_level_set<RealType>(radius, 0.5, 0.5);          
-    // auto level_set_function = flower_level_set<RealType>(radius, 0.5, 0.5, 8, 0.03);            
+    // auto level_set_function = circle_level_set<RealType>(radius, 0.5, 0.5);          
+    auto level_set_function = flower_level_set<RealType>(radius, 0.5, 0.5, 8, 0.03);            
 
     // ##################################################
     // ################################################## Space discretization
