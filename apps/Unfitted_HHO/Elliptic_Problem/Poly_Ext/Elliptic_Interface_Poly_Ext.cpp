f4a3542c29502f645b69400217ede761db6cdc94
Romain Mottier <romain.mottier@outlook.com>
2025-10-01 14:34:47 +0200

    Update Elliptic_Interface_Poly_Ext.cpp

--- apps/Unfitted_HHO/Elliptic_Problem/Poly_Ext/Elliptic_Interface_Poly_Ext.cpp
+++ apps/Unfitted_HHO/Elliptic_Problem/Poly_Ext/Elliptic_Interface_Poly_Ext.cpp
@@ -239,7 +239,7 @@ void CutHHOSecondOrderConvTest(int argc, char **argv) {
             // ########## Level set function
             RealType h = 0.1/std::pow(2,l);
             RealType line_y = 0.5015625; 
-            RealType p = 4.0;
+            RealType p = -4.0;
             RealType radius = 1.0/3.0 + p/32.0;  
             // RealType radius = 1.0/3.0 + p/32.0;  
             // RealType radius = 1.0/3.0 + p/640.0;  
